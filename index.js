958ecc962d950084dec63819d33f0923de8ba2eb
Caleb Maclennan <caleb@alerque.com>
2019-09-11 16:56:35 +0300

    Sketch out a (resource inefficient) way to run fluent from helper

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ exports.fluent = function (key, options) {
   options = options || {};
   options.hash = options.hash || {};
 
-  var language;
+  var language, ftl, result;
 
   if (typeof key !== "string") {
-    throw "{{fluent}} helper: invalid key. Object keys must be formatted as strings.";
+    throw "{{fluent}} helper: invalid key. Keys must be formatted as strings.";
   }
 
   if (typeof options.hash.language === "string") {
@@ -21,13 +21,25 @@ exports.fluent = function (key, options) {
     throw "{{fluent}} helper: the 'language' parameter is not defined.";
   }
 
-  if (typeof this[language] === "undefined") {
-    throw "{{fluent}} helper: context object not defined for language '" + language + "'.";
+  if (typeof options.hash.ftl === "string") {
+    ftl = options.hash.ftl;
+  } else {
+    ftl = this.ftl;
   }
 
-  if (typeof this[language][key] === "undefined") {
-    throw "{{fluent}} helper: property '" + key + "' is not defined for language '" + language + "'.";
+  if (typeof ftl === "undefined") {
+    ftl = language + ".ftl";
   }
 
-  return this[language][key];
+  var source = fs.readFileSync(ftl).toString();
+  var bundle = new fluent.FluentBundle([language]);
+  var resource = new fluent.FluentResource(source);
+  bundle.addResource(resource);
+
+  try {
+    result = bundle.getMessage(key);
+    return result.value;
+  } catch (err) {
+    throw "{{fluent}} helper: translation for '" + key + "' is not available for language '" + language + "'.";
+  }
 };
