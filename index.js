6f1435778c9bae1a146c7f0857e2c3a97cafe39e
Caleb Maclennan <caleb@alerque.com>
2019-09-16 16:31:00 +0300

    Refactor using handlebar-utils functions in preparation for block mode

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
+var utils = require("handlebars-utils");
 var fluent = require("@fluent/bundle");
 var fs = require("fs");
 var err = "{{fluent}} helper: ";
 
-exports.fluent = function (key, options) {
-  var language = this.language, ftl = this.ftl, result, context = this;
+exports.fluent = function (thisArg, locals, options) {
+  options = utils.options(thisArg, locals, options);
+  var context = utils.context(this, locals, options);
 
-  if (typeof key !== "string") {
-    throw new Error(err+"invalid key. Keys must be formatted as strings.");
-  }
+  if (utils.isBlock()) {
 
-  var [key, attr] = key.split('.', 2);
+  } else {
 
-  if (typeof options.hash.language === "string") {
-    language = options.hash.language;
-    delete options.hash.language;
-  }
-
-  if (typeof language === "undefined") {
-    throw new Error(err+"the 'language' parameter is not defined.");
-  }
-
-  if (typeof options.hash.ftl === "string") {
-    ftl = options.hash.ftl;
-    delete options.hash.ftl;
-  }
-
-  if (typeof ftl === "undefined") {
-    ftl = language + ".ftl";
-  }
-
-  Object.keys(options.hash).forEach(function(k) { context[k] = options.hash[k]; });
-
-  var source = fs.readFileSync(ftl).toString();
-  var bundle = new fluent.FluentBundle([language]);
-  var resource = new fluent.FluentResource(source);
-  bundle.addResource(resource);
-
-  try {
-    var message = bundle.getMessage(key);
-    if (typeof attr === "string") {
-      result = message.attributes[attr];
-    } else {
-      result = message.value;
+    if (!utils.isString(thisArg)) {
+      throw new Error(err+"invalid key. Keys must be formatted as strings.");
     }
-    return bundle.formatPattern(result, context);
-  } catch (_) {
-    throw new Error(err+"translation for '" + key + "' is not available for language '" + language + "'.");
+
+    var [key, attr] = thisArg.split('.', 2);
+
+    if (typeof context.language === "undefined") {
+      throw new Error(err+"the 'language' parameter is not defined.");
+    }
+
+    if (typeof context.ftl === "undefined") {
+      context.ftl = context.language + ".ftl";
+    }
+
+    var source = fs.readFileSync(context.ftl).toString();
+    var bundle = new fluent.FluentBundle([context.language]);
+    var resource = new fluent.FluentResource(source);
+    bundle.addResource(resource);
+
+    try {
+      var message = bundle.getMessage(key);
+      result = utils.isString(attr) ?
+        message.attributes[attr] :
+        message.value;
+      return bundle.formatPattern(result, context);
+    } catch (_) {
+      throw new Error(err+"translation for '" + key + "' is not available for language '" + context.language + "'.");
+    }
+
   }
 };
 
